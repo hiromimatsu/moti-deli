7d4ef931ed68930c9f22ed94c2e9c5b63c398216
hiromimatsu <hiro.ay12@icloud.com>
2020-07-30 18:48:30 +0900

    ArticleNewPage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,28 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, expect: :index
+
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:restaurant, :image, :instagram, :menu, :rule, :area_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
