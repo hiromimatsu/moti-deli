93b3583ee5b8902fbfdd933150efe5aae84f467b
hiromimatsu <hiro.ay12@icloud.com>
2020-08-01 23:40:41 +0900

    ajax-change

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,23 +2,13 @@ $(function() {
   let search_list = $(".Article");
 
   function appendArticle(article){
-    if(article.user_sign_in && article.user_sign_in.id == article.user_id){
-      let html = `
-      <div @articles.each do |article| >
-        <a href= image_tag(article.image.url, class: "Article__Image"), article_path(article.id), method: :get>
-        </a>
-      </div>`
+    let html = `
+        <div @articles.each do |article| >
+          <a href= image_tag(article.image.url, class: "Article__Image"), article_path(article.id), method: :get>
+          </a>
+        </div>`
     search_list.append(html);
-    } else {
-      let html = `
-      <div @articles.each do |article| >
-        <a href= image_tag(article.image.url, class: "Article__Image"), article_path(article.id), method: :get>
-        </a>
-      </div>`
-    search_list.append(html);
-    }
   }
-
   function appendErrMsgToHTML(msg) {
     let html = `<div class='name'>${ msg }</div>`
     search_list.append(html);
@@ -33,6 +23,7 @@ $(function() {
       dataType: 'json'
     })
     .done(function(articles) {
+      console.log(articles);
       search_list.empty();
       if (articles.length !== 0) {
         articles.forEach(function(article){
