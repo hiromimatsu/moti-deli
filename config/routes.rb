1f722b6f1428bcbe3a30fd70889f5f8e7496b9c5
hiromimatsu <hiro.ay12@icloud.com>
2020-08-31 21:16:10 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :articles do
     resources :likes, only: [:create, :destroy]
+
     collection do 
       get 'search'
       get 'get_category_children', defaults: { format: 'json' }
     end
+    
     member do 
       get 'get_category_children', defaults: { format: 'json' }
     end
