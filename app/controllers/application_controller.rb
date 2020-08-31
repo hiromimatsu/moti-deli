07a834479b1ac1d13fd9972d2fa1949ce6be7fbb
hiromimatsu <hiro.ay12@icloud.com>
2020-08-31 21:25:16 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,5 @@ class ApplicationController < ActionController::Base
   end
   
   # Basic認証
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+  # http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 end
