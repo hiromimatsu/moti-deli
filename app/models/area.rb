351fa3881766617992824a7804b63e3919e9edcd
hiromimatsu <hiro.ay12@icloud.com>
2020-09-02 11:10:08 +0900

    環状線駅追加

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,3 @@
 class Area < ActiveHash::Base
-  self.data = [{id: 1, name: '大阪'}, {id: 2, name: '福島'}, {id: 3, name: '天満'}, {id: 4, name: '京橋'}, {id: 5, name: '天王寺'}, {id: 6, name: '西九条'}]
+  self.data = [{id: 1, name: '大阪'}, {id: 2, name: '福島'}, {id: 3, name: '天満'}, {id: 4, name: '京橋'}, {id: 5, name: '天王寺'}, {id: 6, name: '西九条'}, {id: 7, name: '野田'}, {id: 8, name: '弁天町'}, {id: 9, name: '大正'}, {id: 10, name: '今宮'}, {id: 11, name: '新今宮'}, {id: 12, name: '寺田町'}, {id: 13, name: '鶴橋'}, {id: 14, name: '玉造'}, {id: 15, name: '大阪城公園'}, {id: 16, name: '桜ノ宮'}, {id: 17, name: '芦原橋'}, {id: 18, name: '桃谷'}, {id: 19, name: '森ノ宮'}]
 end
